0fa85f6d0f81e9c5b19c24d0f8c8f126ff28efb4
Yuuki Aizato <yuukishuuto160318@gmail.com>
2024-01-30 15:34:53 +0900

    Title部分の追加

---
+++ src/components/Title.jsx
@@ -0,0 +1,10 @@
+const Title = () => {
+    return(
+        <header>
+            <h1>画像検索サイト</h1>
+            <p><a href="https://unsplash.com/">UnsplashApi</a></p>
+        </header>
+    );
+}
+ 
+export default Title;
